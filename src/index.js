c378cdcdc2cd2eed2254136dd9efac2ad9a0c85f
Mariano Mendoza <mariano.hw11@gmail.com>
2020-09-16 01:33:33 -0300

    primeras configuraciones

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import path from 'path';
+
+const app = express();
+
+// crear una variable port
+app.set('port', process.env.PORT || 4000 )
+
+app.listen(app.get('port'), () => {
+    console.log(path.join(__dirname, "../public"))
+    console.log("server on port " + app.get('port'));
+})
+
+// Middlewares
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended:true}));
+
+// aqui agrego la carpeta public
+app.use(express.static(path.join(__dirname, "../public")));
+
+// Rutas
+app.get('/', (req, res) => {
+    res.send("hola mundo :D")
+})
+
+app.get('/usuarios', (req, res) => {
+    res.send("estoy en la pagina de usuarios")
+})
